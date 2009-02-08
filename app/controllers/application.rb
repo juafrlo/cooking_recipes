f39f1c365cb554930ff650d09c679ef9b3ab6c03
Juan de Frías <juafrlo@gmail.com>
2009-02-08 21:26:22 +0100

    test commit

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,13 +7,6 @@ class ApplicationController < ActionController::Base
 
   helper :all # include all helpers, all the time
 
-  
-
-
-
-
-
-
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '08168a9889846ca9c99a58dbc1f9411e'
