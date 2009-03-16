479c4510c93de9c9b9a30c0a1742f0e334e12517
Juan de Frías <juafrlo@gmail.com>
2009-03-16 22:11:47 +0100

    changes to migrations

--- db/migrate/20081223075224_add_specific_roles_to_specific_users.rb
+++ db/migrate/20081223075224_add_specific_roles_to_specific_users.rb
@@ -2,11 +2,11 @@ class AddSpecificRolesToSpecificUsers < ActiveRecord::Migration
   def self.up
     @u1 = User.find(1);
     @u2 = User.find(2);
-    @u3 = User.find(3);
+    #@u3 = User.find(3);
       
     @u1.roles << Role.find([1])
     @u2.roles << Role.find([2])
-    @u3.roles << Role.find([3])
+    #@u3.roles << Role.find([3])
 
 
   end
@@ -14,3 +14,4 @@ class AddSpecificRolesToSpecificUsers < ActiveRecord::Migration
   def self.down
   end
 end
+20081223075223
