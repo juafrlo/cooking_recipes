d7790d285baafc541525363ada0d1d3a1e98a5a2
Juan de Frías <juafrlo@gmail.com>
2009-11-19 18:45:05 +0100

    Must fill comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ActiveRecord::Base
-
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
   belongs_to :recipient, :class_name => "User", :foreign_key => "recipient_id"
 
   after_create :send_notification_to_owner
+  validates_presence_of :comment
   
   def send_notification_to_owner
     if self.recipient.receive_comments_emails && self.user != self.recipient
