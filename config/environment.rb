01ee165769b55cf747c5b158057db72c2f391b47
Juan de Frías <juafrlo@gmail.com>
2009-11-27 08:13:50 +0100

    New error pages and new config for production

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
-if RAILS_ENV == production 
-  ENV["GEM_PATH"]="/home/USUARIO/ruby/gems:/usr/local/lib/ruby/gems/1.8"
+if ENV['RAILS_ENV'] == 'production' 
+  ENV["GEM_PATH"]="/home/antesdec/ruby/gems:/usr/local/lib/ruby/gems/1.8"
 end
 
 # Specifies gem version of Rails to use when vendor/rails is not present
