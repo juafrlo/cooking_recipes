dbd33b801c9a1eeb4e6c7af89822a2c725b073c2
juafrlo <juafrlo@gmail.com>
2010-11-30 20:26:38 +0100

    Show tags as links on advice show

--- app/helpers/advices_helper.rb
+++ app/helpers/advices_helper.rb
@@ -1,2 +1,11 @@
 module AdvicesHelper
+  def advices_tags(advice)
+    html = ""
+    advice.tags.each do |tag|
+      html += link_to(tag.name, tag_path(tag))
+      html += ", " unless tag == advice.tags.last
+    end
+    html
+  end
+  
 end
