46c17dfb6ce34074f97a05075642eceffe0e0e9e
juafrlo <juafrlo@gmail.com>
2011-07-17 21:04:35 +0200

    Fixed date formats

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -87,9 +87,9 @@ module ApplicationHelper
 
   def date_block(date)
     html = "<div class='dateblock'>"
-    html +=  "<span class='dateblock_mon'>#{l(date, :format => 'month')} </span>"
-    html +=  "<span class='dateblock_day'>#{l(date, :format => 'day')} </span>"
-    html +=  "<span class='dateblock_year'>#{l(date, :format => 'year')}</span>"
+    html +=  "<span class='dateblock_mon'>#{l(date, :format => :month)} </span>"
+    html +=  "<span class='dateblock_day'>#{l(date, :format => :day)} </span>"
+    html +=  "<span class='dateblock_year'>#{l(date, :format => :year)}</span>"
     html += "</div>"
     html
   end
