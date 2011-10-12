d2f784089b17333817ca729c244b8c2f841120e7
juafrlo <juafrlo@gmail.com>
2011-10-12 13:54:38 +0200

    Fixed email

--- config/initializers/mail.rb
+++
@@ -1,9 +0,0 @@
-require "smtp_tls"
- 
-ActionMailer::Base.delivery_method = :sendmail
-ActionMailer::Base.smtp_settings = {
-:address => "smtp.gmail.com",
-:port => 587,
-:authentication => :plain,
-:user_name => "you@gmail.com",
-:password => 'yourpassword'}
