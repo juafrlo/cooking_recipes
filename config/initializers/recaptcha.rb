249b40b787a7ec3b6ca6c7edb4270f1fe08e3d31
juafrlo <juafrlo@gmail.com>
2011-10-12 13:41:59 +0200

    Updated recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LdJB8kSAAAAAMoqlCDqMHbx9mDgXw4CRyh5CzI2'
-  config.private_key = '6LdJB8kSAAAAAL2NbgKerAjEC-2CZzwezriQNSBb'
+  config.public_key  = '6Ld_igkAAAAAAF0pfRNL7Demw5SOWgEAqpWVUKMx'
+  config.private_key = '6Ld_igkAAAAAAIDrGuHJ0DZzmPNEhgo1zEq06Jau'
 end
