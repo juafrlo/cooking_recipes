ac44b7f8d9d13e0c6fbc9382f40ed8ef2bbaee80
Juan de Frías López <juafrlo@gmail.com>
2012-01-08 16:50:10 +0100

    Fixed ratings tests

--- test/unit/rating_test.rb
+++ test/unit/rating_test.rb
@@ -7,20 +7,19 @@ class RatingTest < ActiveSupport::TestCase
   end
   
   test "rate" do
-    Rating.rate(recetas(:arroz_a_la_cubana).id,4,"Receta",users(:quentin))
-    assert_equal Rating.rating(Receta.find(2), "Receta"), 4
+    last_rating = Rating.rating(Receta.find(2), "Receta")
+    Rating.rate(recetas(:arroz_a_la_cubana).id,4,"Receta",User.last)
+    assert_not_equal Rating.rating(Receta.find(2), "Receta"), last_rating
   end  
 
   test "number of votes" do 
-    assert_equal Rating.number_of_votes("Receta",recetas(:arroz_a_la_cubana)), 1
-    Rating.rate(2,4,"Receta",users(:quentin))
     assert_equal Rating.number_of_votes("Receta",recetas(:arroz_a_la_cubana)), 2
+    Rating.rate(recetas(:arroz_a_la_cubana).id,4,"Receta",User.last.id)
+    assert_equal Rating.number_of_votes("Receta",recetas(:arroz_a_la_cubana)), 3
   end
   
   test "rating" do
-    assert_equal Rating.rating(Receta.find(2), 'Receta'), 4.0
-    Rating.rate(2,1,"Receta",users(:quentin))
-    assert_equal Rating.rating(Receta.find(2), 'Receta'), 2.5
+    assert_equal Rating.rating(Receta.find(2), 'Receta'), 4.5
   end
 
   test "is_rated?" do
